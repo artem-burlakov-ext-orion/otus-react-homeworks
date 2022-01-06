4106073db7a04a5700881f0c1a3e8da6d1bdedea
Artem Burlakov <artem.burlakov@orioninc.com>
2022-01-07 01:08:14 +0300

    initial

--- hw1/jest.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  'preset': 'ts-jest',
-  'coverageDirectory': './coverage',
-  // "testMatch": [
-  //   "**/?(*.)+test.ts"
-  // ]
-};
--- hw1/src/index.ts
+++
@@ -1,3 +0,0 @@
-import runCalculator from './calcEngine';
-
-runCalculator();
